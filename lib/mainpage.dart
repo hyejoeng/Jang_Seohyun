6b868fea923998ec7b340c83ddfae1723ba6217a
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2023-11-26 23:31:22 +0900

    mainpage 제작중

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:jang_seohyun/context_utils.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
-        body: Text('메인화면',),
+  Widget build(BuildContext context) => PopScope(
+        canPop: false,
+        child: SafeArea(
+          child: Scaffold(
+            body: SizedBox(
+                width: double.infinity,
+                height: screensize(context).height * 0.8,
+                child: Container(color: Colors.amberAccent,)),
+            bottomNavigationBar: SizedBox(
+              width: double.infinity,
+              height: screensize(context).height * 0.2,
+              child: Container(
+                color: Colors.deepOrange,
+              ),
+            )
+          ),
+        ),
       );
 }
