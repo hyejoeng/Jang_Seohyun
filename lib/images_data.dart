0750901a7f8b95cb0f4c64a6820101bd65aa0970
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2023-12-11 04:28:52 +0900

    mainpage 제작중

---
+++ lib/images_data.dart
@@ -0,0 +1,16 @@
+class ImagesData {
+  final String imagesUrl;
+  final String dateData;
+
+  ImagesData({
+    required this.imagesUrl,
+    required this.dateData,
+  });
+}
+
+List<ImagesData> ImagesDataList = [
+  ImagesData(imagesUrl: 'asset/images/jangSeohyun_images4.jpg', dateData: '10월 19일'),
+  ImagesData(imagesUrl: 'asset/images/jangSeohyun_images3.jpg', dateData: '10월 25일'),
+  ImagesData(imagesUrl: 'asset/images/jangSeohyun_images1.jpg', dateData: '10월 31일'),
+  ImagesData(imagesUrl: 'asset/images/jangSeohyun_images2.jpg', dateData: '10월 31일'),
+];
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jang_seohyun/context_utils.dart';
+import 'package:jang_seohyun/images_data.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -17,30 +18,50 @@ class MainPage extends StatelessWidget {
                   Container(
                       alignment: Alignment.center,
                       width: screensize(context).width,
-                      height: screensize(context).height * 0.2,
+                      height: screensize(context).height * 0.15,
                       child: Text(
                         '장서현의 사진첩',
                         style: textTheme(context).bodyMedium,
                         textAlign: TextAlign.center,
                       )),
                   Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                // width: screensize(context).width * 0.2,
-                                child: Image.asset('asset/images/jangSeohyun_images1.jpg'),
+                        child: ListView.builder(
+                          itemCount: ImagesDataList.length,
+                          itemBuilder: (context, index) => SingleChildScrollView(
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                BorderRadius.vertical(top: Radius.circular(50)),
+                                color: Colors.blue,
                               ),
-                              Text('10월 31일', style: textTheme(context).titleMedium,)
-                            ],
-                          )
-
-                        ],
-                      ),
-                    ),
-                  )
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 5),
+                                  Padding(
+                                    padding: const EdgeInsets.all(20),
+                                    child: Row(
+                                      children: [
+                                        ClipRRect(
+                                          child: Image.asset(
+                                            ImagesDataList[index].imagesUrl,
+                                            width: screensize(context).width * 0.3,
+                                          ),
+                                          borderRadius: BorderRadius.circular(30),
+                                        ),
+                                        Text(
+                                          ImagesDataList[index].dateData,
+                                          style: textTheme(context).titleMedium,
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                          ),
+                        )),
+                  ),
                 ],
               ),
             ),
