0a0b03401e333c37b484acbc110c2082ecd9e6b4
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2023-12-11 20:30:03 +0900

    2023-12-11 앱 이름 바꿈

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -25,53 +25,26 @@ class MainPage extends StatelessWidget {
                         textAlign: TextAlign.center,
                       )),
                   Expanded(
-                        child: ListView.builder(
-                          itemCount: ImagesDataList.length,
-                          itemBuilder: (context, index) => SingleChildScrollView(
-                            child: Container(
-                              width: double.infinity,
-                              height: double.infinity,
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                BorderRadius.vertical(top: Radius.circular(50)),
-                                color: Colors.blue,
-                              ),
-                              child: Column(
-                                children: [
-                                  SizedBox(height: 5),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20),
-                                    child: Row(
-                                      children: [
-                                        ClipRRect(
-                                          child: Image.asset(
-                                            ImagesDataList[index].imagesUrl,
-                                            width: screensize(context).width * 0.3,
-                                          ),
-                                          borderRadius: BorderRadius.circular(30),
-                                        ),
-                                        Text(
-                                          ImagesDataList[index].dateData,
-                                          style: textTheme(context).titleMedium,
-                                        )
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
+                    child: SingleChildScrollView(
+                      child: Container(
+                        width: double.infinity,
+                        height: screensize(context).height,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+                          color: Colors.blueAccent
+                        ),
+                        child: Center(
+                          child: Text(
+                            'MainPage',
+                            textAlign: TextAlign.center,
                           ),
-                        )),
+                        ),
+                      ),
+                    )
                   ),
                 ],
               ),
             ),
-            // bottomNavigationBar: SizedBox(
-            //   width: double.infinity,
-            //   height: screensize(context).height * 0.2,
-            //   child: Container(
-            //     color: Colors.deepOrange,
-            //   ),
-            // )
           ),
         ),
       );
